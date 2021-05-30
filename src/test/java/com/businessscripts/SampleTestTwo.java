a6c099566bbaa357db5ef0c47d570bf44cd92e00
srkrishna3384 <srkrishna3384@gmail.com>
2021-05-30 17:30:38 +0530

    Added sampletesttwo file

---
+++ src/test/java/com/businessscripts/SampleTestTwo.java
@@ -0,0 +1,25 @@
+package com.businessscripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTestTwo {
+	@Test
+public static void testTwo() throws InterruptedException {
+
+//	System.setProperty("", "");
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	System.out.println("Browser launched successfully...");
+	driver.manage().window().maximize();
+	System.out.println("Maximized the browser...");
+	driver.get("https://www.flipkart.com");
+	System.out.println("Navigated to "+driver.getCurrentUrl());
+	Thread.sleep(5000);
+	driver.close();
+
+}
+}
